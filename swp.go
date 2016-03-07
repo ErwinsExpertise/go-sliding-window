eb26713b5afabe68e136b1899f87c11eb2e094b6
Jason E. Aten <j.e.aten@gmail.com>
2016-03-07 01:57:36 -0800

    atg. docs++

--- swp.go
+++ swp.go
@@ -26,7 +26,7 @@ control between exactly two nats endpoints; in many
 cases this is sufficient to allow all subscribers to
 keep up.  If you have a wide variation in consumer
 performance, establish the rate-controlling
-`swp` Session between your producer and your
+swp Session between your producer and your
 slowest consumer.
 
 There is also a Session.RegisterAsap() API that can be
